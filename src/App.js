174517e8dce01261e9c8f9736b7707f864df7b36
Nasri <nasrishifar@gmail.com>
2025-05-02 15:54:06 +0530

    add home component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Home />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
